b9835f2c98c076d0fdb68162edc7fa1504a6c918
Pedro Leal <leal26@tamu.edu>
2018-11-03 20:27:30 -0500

    Added Hausdorff distance (automatic crowding distance) and plotting

--- exploration.py
+++ exploration.py
@@ -1,4 +1,4 @@
-from scipy.optimize import differential_evolution
+from scipy import spatial, stats
 import numpy as np
 
 
@@ -7,25 +7,84 @@ class Explore():
         self.constraints = constraints
         self.n_results = n_results
 
-    def random_selection(self, bounds=None):
-        self.output = []
+    def _scale2bounds(self, x, bounds):
+        if bounds is not None:
+            for i in range(self.constraints.n_dim):
+                try:
+                    x[:, i] = bounds[i][0] + x[:, i]*(bounds[i][1] -
+                                                      bounds[i][0])
+                except(TypeError):
+                    x[i] = bounds[i][0] + x[i]*(bounds[i][1] - bounds[i][0])
+        return x
+
+    def _crowding_constraint(self, x, crowding_distance, counter):
+        self.crowding_distance = crowding_distance
+        if crowding_distance is None or counter != 0:
+            return True
+        else:
+            norms = np.linalg.norm(np.array(self.output) - np.array(x), axis=1)
+            distance = min(norms)
+
+            if type(crowding_distance) == str:
+                crowding_distance = spatial.distance.directed_hausdorff(np.array(self.output),
+                                                                        np.array([x]))[0]
+            return distance >= crowding_distance
+
+    def random(self, bounds=None, crowding_distance=None):
+        self.output = np.zeros((self.n_results, self.constraints.n_dim))
         counter = 0
         while counter != self.n_results:
             x_try = np.random.rand(self.constraints.n_dim)
-            if bounds is not None:
-                for i in range(self.constraints.n_dim):
-                    x_try[i] = bounds[i][0] + x_try[i]*(bounds[i][1] -
-                                                        bounds[i][0])
-            if self.constraints.apply(x_try):
-                self.output.append(x_try)
+            x_try = self._scale2bounds(x_try, bounds)
+            crowding_constraint = self._crowding_constraint(x_try,
+                                                            crowding_distance,
+                                                            counter)
+            if self.constraints.apply(x_try) and crowding_constraint:
+                self.output[counter, :] = x_try
                 counter += 1
 
-    def genetic_exploration(self, bounds=None):
-        if bounds is None:
-            bounds = self.constraints.n_dim*[[0, 10], ]
-        print(bounds)
-        return differential_evolution(self._func, bounds,
-                                      popsize=self.n_results)
+    def genetic(self, bounds=None):
+        from pyDOE import lhs
+        # start with a random population
+        self.output_try = lhs(self.constraints.n_dim,
+                              samples=self.n_results,
+                              criterion='center')
+        self.output_try = self._scale2bounds(self.output_try, bounds)
+        satisfy_constraints = []
+        for x in self.output_try:
+            satisfy_constraints.append(self.constraints.apply(x))
+            print(x)
+            if self.constraints.apply(x):
+                print('ALKJAS')
+        print(satisfy_constraints)
 
     def write2file(self, filename='output.txt'):
         np.savetxt(filename, self.output, delimiter='\t')
+
+    def plotting(self, index=(0, 1), grid_size=(100, 100),
+                 plot_training=False, plot_grid=False):
+        import matplotlib.pyplot as plt
+        x = self.output[:, index[0]]
+        y = self.output[:, index[1]]
+        xmin, xmax = min(x), max(x)
+        ymin, ymax = min(y), max(y)
+
+        self.kernel = stats.gaussian_kde(self.output[:, index].T)
+        X, Y = np.mgrid[xmin:xmax:complex(grid_size[0]),
+                        ymin:ymax:complex(grid_size[1])]
+        positions = np.vstack([X.ravel(), Y.ravel()])
+        P = np.reshape(self.kernel(positions).T, X.shape)
+
+        # P = self.kernel(A.flatten(), V.flatten())
+
+        plt.figure()
+        plt.contourf(X, Y, P)
+        if plot_training:
+            plt.scatter(x, y)
+        if plot_grid:
+            plt.scatter(X, Y)
+        plt.colorbar()
+        plt.xlabel('Input %i' % index[0])
+        plt.ylabel('Input %i' % index[1])
+        plt.title(self.crowding_distance)
+        # plt.show()
--- sampler.py
+++ sampler.py
@@ -1,12 +1,32 @@
+import matplotlib.pyplot as plt
 import time
 from constraints import Constraint
 from exploration import Explore
 
+
 c = Constraint(r'.\formulation.txt')
 e = Explore(c, 1000)
 
-start = time.time()
-e.random_selection()
-e.write2file()
-end = time.time()
-print(end - start)
+# start = time.time()
+# # e.random()
+# e.random(crowding_distance=0.1)
+# # e.write2file()
+# end = time.time()
+# print(end - start)
+#
+# plt.figure()
+# x, y = e.output.T
+# plt.scatter(x, y)
+#
+# e.random(crowding_distance='Hausdorff')
+#
+# x, y = e.output.T
+# plt.scatter(x, y, c='r')
+# plt.show()
+
+e.random(crowding_distance=0.1)
+e.plotting(index=(2, 3))
+
+e.random(crowding_distance='Hausdorff')
+e.plotting(index=(2, 3))
+plt.show()
